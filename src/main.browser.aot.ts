96d52b676a8e9e8fb71c8fd7758f2d689de9ce60
yeluoqiuzhi <827884374@qq.com>
2016-09-19 15:15:33 +0800

    enableProdMode() on aot entry

--- src/main.browser.aot.ts
+++ src/main.browser.aot.ts
@@ -1,4 +1,6 @@
 import { platformBrowser } from '@angular/platform-browser';
 import { AppModuleNgFactory } from './app/app.module.ngfactory';
+import { enableProdMode } from '@angular/core';
 
+enableProdMode();
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
